e7334782517f179458c9781eba5d56ea815ad343
varizz <varizky16@gmail.com>
2023-09-11 18:17:06 +0700

    few changes

--- src/js/validity.js
+++ src/js/validity.js
@@ -1,8 +1,10 @@
-
 function InvalidMsg(textbox) {
-    if (textbox.value === '') {
-        textbox.setCustomValidity
-              ('Please Fill Your Name First.');
+    if (textbox.name ==='Name' && textbox.value === '') {
+        textbox.setCustomValidity('Please Fill Your Name First.');
+    } else if (textbox.name === 'Email' && textbox.value === '') {
+        textbox.setCustomValidity('Please Fill Your Email First.'); 
+    } else if (textbox.name === 'Message' && textbox.value === '') {
+        textbox.setCustomValidity('Please Fill your Nice Message here.');
     } else {
         textbox.setCustomValidity('');}
     return true;}
